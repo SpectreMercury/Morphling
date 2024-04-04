4de4f5e226df60d327d4fd3603bb178ee0b8b8b1
Soul_Cai <5113047+CooCoode@users.noreply.github.com>
2024-04-04 10:20:02 +0800

    add test

--- test/morphling.test.ts
+++ test/morphling.test.ts
@@ -1,11 +1,20 @@
-import CKB from "../src/morphling"
+// import CKB from "../src/morphling"
+import { capacity_createRawTransactionForJoyID } from '../src/Transaction/index'
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    CKB();
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    // CKB();
+    capacity_createRawTransactionForJoyID(
+      'ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqqyse9w5xpgupt7q6sru8dcq9q8k4lktsegcsxjqm',
+      'ckt1qrfrwcdnvssswdwpn3s9v8fp87emat306ctjwsm3nmlkjg8qyza2cqgqqyse9w5xpgupt7q6sru8dcq9q8k4lktsegcsxjqm',
+      100,
+      {
+        RPCUrl: 'https://testnet.ckb.dev'
+      }
+    )
     expect(true).toBeTruthy()
   })
 })
